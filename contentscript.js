fa3a1915da5466931b0f00754496fa024a76fabc
unknown <Administrator@.(none)>
2010-06-04 03:59:39 +0900

    initial commit.

---
+++ contentscript.js
@@ -0,0 +1,27 @@
+{
+if(webkitNotifications) {
+	var img = document.createElement("img");
+	img.src = chrome.extension.getURL("notifyMessage.png");
+	img.alt = "popup notify";
+	var na = document.createElement("a");
+	na.setAttribute("class", "disabled");
+	na.href = "#";
+	na.title = "turn on popup notification";
+	na.setAttribute("onclick","lingr.ui.togglePopup(); return false;");
+	na.appendChild(img);
+	
+	var div = document.createElement("div");
+	div.id = "popupNotify";
+	div.appendChild(na);
+	
+	var avatar = document.getElementById("avatar");
+	avatar.insertBefore(div, document.getElementById("sound"));
+	
+
+	var script = document.createElement("script");
+	script.src = chrome.extension.getURL("onMessage.js");
+	script.type = "text/javascript";
+	document.body.appendChild(script);
+}
+}
+
---
+++ onMessage.js
@@ -0,0 +1,47 @@
+if(lingr.getCookie('popup')) {
+	if(webkitNotifications.checkPermission() == 0) {
+		var target = $('popupNotify').down('a');
+		target.removeClassName('disabled').addClassName('enabled');
+		target.title = 'turn off popup notification';
+	} else {
+		lingr.clearCookie('popup');
+	}
+}
+
+lingr.ui.togglePopup = function() {
+	var target = $('popupNotify').down('a');
+	if (target.hasClassName('disabled')) {
+		if( webkitNotifications.checkPermission() != 0) {
+			webkitNotifications.requestPermission();
+		}
+		if(webkitNotifications.checkPermission() == 0) {
+			target.removeClassName('disabled').addClassName('enabled');
+			target.title = 'turn off popup notification';
+			lingr.setCookie('popup','on');
+		}
+	} else {
+		target.removeClassName('enabled').addClassName('disabled');
+		target.title = 'turn on popup notification';
+		lingr.clearCookie('popup');
+	}
+};
+
+lingr.ui._insertMessage = lingr.ui.insertMessage;
+lingr.ui.insertMessage = function(msg, bulk) {
+	notifyMessage(msg);
+	this._insertMessage(msg, bulk);
+}
+
+
+function notifyMessage(msg) {
+	if(lingr.getCookie("popup") && webkitNotifications.checkPermission() == 0) {
+		var notification = webkitNotifications.createNotification(
+			msg.icon_url, msg.nickname, msg.text
+		);
+		notification.show();
+		
+		setTimeout(function() {
+			notification.cancel();
+		}, 4000);
+	}
+}
